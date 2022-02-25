1d87264be9f3716935d12791b82a2ea64fff1954
Ronald Pereira <ronaldpereira01@gmail.com>
2022-02-24 20:13:05 -0400

    Added tailwind typography, line-clamp and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import type { NextPage } from 'next'
-import Layout from "../components/Layout"
+import type { NextPage } from 'next';
+import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
       <main className="lg:col-span-full">
-        <div className="bg-red-600 h-2" />
+        <div className="h-2 bg-red-600" />
       </main>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
